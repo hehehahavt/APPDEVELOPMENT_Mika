539f1d147b6fc0803085eea99edfe43e075eee60
hehehahavt <anonyclone003@gmail.com>
2017-04-10 00:50:05 +0300

    Update wave.h

--- wave.h
+++ wave.h
@@ -1,12 +1,12 @@
+/* This file contains wave header information as structure
+ * it also contains neccessary constant definition of thi
+ */
 
-// this header contains wave header information as a struct
-// it also contains necessary constant definitions and functions
-// prototypes used in this project 
 typedef struct {
 	char ChunkID[4];
 	int ChunkSize;
 	char Format[4];
-	char Subchunk1id[4];
+	char Subchunk1ID[4];
 	int Subchunk1Size;
 	short int AudioFormat;
 	short int NumChannels;
@@ -15,14 +15,15 @@ typedef struct {
 	short int BlockAlign;
 	short int BitsPerSample;
 	char Subchunk2ID[4];
-	int Subchunk2Size; //data size
-}WAVHDR; //Wave header
+	int Subchunk2Size;
+}WAVHDR;
 //#define DEBUG
+
 #define SAMPLE_RATE 16000
 #define PI 3.14
-//function prototypes
-void displayWAVHDR(WAVHDR hdr); 
+//function prototype
+void displayWAVHDR(WAVHDR hdr);
 void printID(char *);
+void fillID(const char *, char[]);
 void testTone(int freq, double duration);
-void fillID(const char *,char []);
 void displayWAVdata(short int []);
