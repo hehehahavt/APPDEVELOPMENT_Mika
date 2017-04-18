383022da5c14f098ad403c4ab03c3c18cc62fbf1
hehehahavt <anonyclone003@gmail.com>
2017-04-18 08:50:02 +0300

    Update comm.h

--- comm.h
+++ comm.h
@@ -1,6 +1,6 @@
 //in this header we define some neccessary constants for CURL communication
 
 #define COMM
-#define URL "http://www.cc.puv.fi/~e1601114/php/sound.php"
+#define URL "http://www.cc.puv.fi/~e1601114/sound.php"
 
 void send_data_curl(double []);
